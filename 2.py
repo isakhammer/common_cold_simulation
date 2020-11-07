89736b7971af855be0b98a68c5e379be5cd82cb4
sirTh0r <tbaadsvik@gmail.com>
2020-11-07 16:28:59 +0100

    Included Var(y)
    Covariance matrices are now based on Var(y)

--- 2.py
+++ 2.py
@@ -1,76 +1,70 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
+from scipy.spatial import distance_matrix as dm
+
+def transpose_1d_arrs(arrs):
+    return [arr.reshape(len(arr), 1) for arr in arrs]
 
 
-def gauss_process(t_a, mu_a, mu_b, t_b, x_b, sigma=0.5**2, phi=15):
-
-    # Difference Matrix
-    I_a = np.ones(t_a.shape)
-    I_b = np.ones(x_b.shape)
-
-    H_a = np.abs(t_a @ I_a.T - I_a @ t_a.T)
-    H_b = np.abs(t_b @ I_b.T - I_b @ t_b.T)
-    H_ab = np.abs(t_a @ I_b.T - I_a @ t_b.T)
-
-    def corr(d):
-        return (1 + phi*d)*np.exp(-phi*d)
-
-    # Build covariance matrix
-    Sigma_a = corr(H_a)
-    Sigma_b = corr(H_b)
-    Sigma_ab = corr(H_ab)
-
-    Sigma_b_inv = np.linalg.inv(Sigma_b)
-
-    E_ab = mu_a + Sigma_ab @ Sigma_b_inv @ (x_b - mu_b)
-    var_ab = Sigma_a - Sigma_ab @ Sigma_b_inv @ Sigma_ab.T
-    return E_ab, var_ab
+def untranspose_1d_arrs(arrs):
+    return [arr[:, 0] for arr in arrs]
 
 
-# Grid
-t_0 = 0.25
-t_1 = 0.50
-E = 0.5
-n = 51
-th = np.linspace(t_0, t_1,  n  )
-th = th[:, np.newaxis]
-E_y = 0.5*np.ones(th.shape)
+def matern_15(d):
+    return (1 + 15*d)*np.exp(-15*d)
 
-# Given data
-y_b =     np.array([ 0.5, 0.32, 0.40, 0.35, 0.60])
-y_b = y_b[:, np.newaxis]
 
-th_b =  np.array([   0.3, 0.35, 0.39, 0.41, 0.45])
-th_b = th_b[:, np.newaxis]
-E_y_b = E*np.ones(y_b.shape)
-E, Var = gauss_process(t_a=th, mu_a=E_y, t_b=th_b, mu_b=E_y_b, x_b=y_b)
-E = E[:,0]
-th = th[:,0]
-Var_diag = np.diagonal(Var)
-E_l = E - Var_diag*1.645
-E_u = E + Var_diag*1.645
-plt.scatter( th_b, y_b, label = "Data")
-plt.plot( th, E, label = "Expected values" )
-plt.fill_between(th, E_l, E_u, color="gray", alpha  = 0.5)
-plt.xlabel(r"$\theta$", size=20)
-plt.ylabel(r'$y(\theta)$', size=20)
-plt.title("Mean vector and 90% Prediction interval", size=15)
-plt.savefig("gp.png")
-plt.grid()
-plt.legend()
-plt.show()
+def gauss_process(mu_A, mu_B, t_A, t_B, y_B, corr=matern_15, Var=0.5**2):
+    mu_A, mu_B, t_A, t_B, y_B = transpose_1d_arrs([mu_A, mu_B, t_A, t_B, y_B])
+    H_A, H_B, H_AB = [dm(t[0], t[1], p=1) for t in [(t_A, t_A), (t_B, t_B), (t_A, t_B)]]
+    Sigma_A, Sigma_B, Sigma_AB = [Var * corr(H) for H in [H_A, H_B, H_AB]]
+    Sigma_B_inv = np.linalg.inv(Sigma_B)
+    mu_AcB = (mu_A + Sigma_AB @ Sigma_B_inv @ (y_B - mu_B))[:, 0]
+    Sigma_AcB = Sigma_A - Sigma_AB @ Sigma_B_inv @ Sigma_AB.T
+    return mu_AcB, Sigma_AcB
 
-p = norm(loc = E, scale = np.sqrt(Var_diag)).cdf(0.3)
 
-plt.plot( th, p, label = "Expected values" )
-plt.xlabel(r"$\theta$", size=20)
-plt.ylabel(r'$p(\theta)$', size=20)
-plt.title("Probability " + r'$Y(\theta) < 0.3 $', size=15)
-plt.savefig("prob_2.png")
-plt.grid()
-plt.legend()
-plt.show()
+def generate_plots(E, t_A, t_B, y_B):
+    mu_A, mu_B = E * np.ones_like(t_A), E * np.ones_like(t_B)
+    mu_AcB, Sigma_AcB = gauss_process(mu_A, mu_B, t_A, t_B, y_B, matern_15)
+    var_A = np.diagonal(Sigma_AcB)
+    mu_AcB_l = mu_AcB - var_A * 1.645
+    mu_AcB_u = mu_AcB + var_A * 1.645
+    fig, (ax0, ax1) = plt.subplots(1, 2)
+    ax0.scatter(t_B, y_B, label="Data")
+    ax0.plot(t_A, mu_AcB, label=r'\mu_{A \mid B}')
+    ax0.fill_between(t_A, mu_AcB_l, mu_AcB_u, color="gray", alpha=0.5)
+    ax0.set_xlabel(r"$\theta$", size=20)
+    ax0.set_ylabel(r'$y(\theta)$', size=20)
+    ax0.set_title("Mean vector and 90% Prediction interval", size=15)
+    #ax0.savefig("gp.png")
+    ax0.grid()
+    ax0.legend()
+
+    p = norm(loc=mu_AcB, scale=np.sqrt(var_A)).cdf(0.3)
+
+    ax1.plot(t_A, p, label="Expected values")
+    ax1.set_xlabel(r"$\theta$", size=20)
+    ax1.set_ylabel(r'$p(\theta)$', size=20)
+    ax1.set_title("Probability " + r'$Y(\theta) < 0.3 $', size=15)
+    #ax1.savefig("prob_2.png")
+    ax1.grid()
+    ax1.legend()
+    plt.show()
+
+# 5 evaluation points
+t_0, t_1, E, n = 0.25, 0.50, 0.5, 51
+t_A = np.linspace(t_0, t_1, n)
+t_B = np.array([0.3, 0.35, 0.39, 0.41, 0.45])
+y_B = np.array([0.5, 0.32, 0.40, 0.35, 0.60])
+generate_plots(E, t_A, t_B, y_B)
+
+# 6 evaluation points
+t_A = np.linspace(t_0, t_1, n).reshape(n, 1)
+t_B = np.array([0.3, 0.33, 0.35, 0.39, 0.41, 0.45]).reshape(6, 1)
+y_B = np.array([0.5, 0.4, 0.32, 0.40, 0.35, 0.60]).reshape(6, 1)
+generate_plots(E, t_A, t_B, y_B)
 
 
 
